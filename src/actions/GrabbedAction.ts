c6c31a69852893edd7aa62d64e8865d484139703
Christian Michael Baum <mail@christianbaum.com>
2021-09-10 19:30:39 -0700

    Fix kitties can't be grabbed

--- src/actions/GrabbedAction.ts
+++ src/actions/GrabbedAction.ts
@@ -6,18 +6,9 @@ import { ActionHelpers } from "./ActionHelpers";
 
 export class GrabbedAction implements Action {
     do(kitty: StrayKittyState, dt: number) {
-        if (kitty.y < window.innerHeight - 32) {
-            kitty.yVector += dt / 250;
-        } else {
-            if (Math.abs(kitty.yVector) < dt / 100) {
-                kitty.y = window.innerHeight - 32;
-                kitty.action = new StandingAction();
-            }
-            kitty.yVector *= -0.5;
-        }
     }
 
-    readonly type = ActionType.Falling;
+    readonly type = ActionType.Grabbed;
 
     readonly frames = [1, 2, 3];
 }
